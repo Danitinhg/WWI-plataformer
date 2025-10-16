15a6da082e82c897b9c6c76edd0df658e0a9868b
Danitinhg <dani111hg@gmail.com>
2025-10-16 21:51:59 +0200

    Avance en el desarrollo de los scripts: game_manager, level_data y player_data

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,25 +1,60 @@
 extends CharacterBody2D
 
+#Movimiento básico del player, moverse y saltar
+@export var speed: float = 200.0
+@export var jump_velocity: float = -400.0
+@export var gravity: float = 980.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+#Giro en el aire (¡Viiup!)
+@export var spin_duration: float = 0.5
+@export var spin_gravity_multiplier: float = 0.3
+var is_spinning: bool = false
+var spin_timer: float = 0.0
+var can_spin: bool = false
 
+#Habilidades de los niveles
+var abilities: Array = []
 
-func _physics_process(delta: float) -> void:
+@onready var sprite = $Sprite2D
+
+func _ready():
+	add_to_group("player")
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		if is_spinning:
+			velocity.y += gravity * spin_gravity_multiplier * delta
+		else:
+			velocity.y += gravity * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#Al tocar el suelo, el jugador tiene disponible de nuevo el giro
+		can_spin = false
+		is_spinning = false
 
+	#Saltar
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+		can_spin = true
+	
+	#Giro en el aire (pulsar de nuevo el boton de salta en el aire)
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and can_spin and not is_spinning:
+		start_spin()
+	
+	#Movimiento horizontal
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
 	move_and_slide()
+	
+func start_spin():
+		is_spinning = true
+		can_spin = true
+		spin_timer = spin_duration
+		#Cuando se usen los sprites poner en esta linea que gire el player
+
+func collect_item(collectible):
+	PlayerData.add_collectible(collectible.collectible_id)
--- scripts/autoloads/game_manager.gd
+++ scripts/autoloads/game_manager.gd
@@ -1,1 +1,40 @@
 extends Node
+
+#Comunicación entre nodos
+signal level_completed(level_id: int)
+signal ability_unlocked(ability_name: String)
+
+var levels: Array[LevelData] = []
+var current_level: int = -1
+
+func _ready():
+	load_levels()
+
+func load_levels():
+	levels = [
+		load("res://resources/levels/level_01_data.tres"),
+		load("res://resources/levels/level_02_data.tres"),
+		load("res://resources/levels/level_03_data.tres")
+	]
+
+func load_level(level_id: int):
+	current_level = level_id
+	var level_data = levels[level_id]
+	get_tree().change_scene_to_file(level_data.scene_path)
+
+func complete_level(level_id: int):
+	levels[level_id].completed = true
+   
+	# Desbloquear habilidad
+	var ability = levels[level_id].unlocks_ability
+	if ability != "":
+		PlayerData.unlock_ability(ability)
+		ability_unlocked.emit(ability)
+	
+	level_completed.emit(level_id)
+	PlayerData.save_game()
+
+#func return_to_level_selector():
+	#get_tree().change_scene_to_file("res://scenes/ui/le")
+	
+	
