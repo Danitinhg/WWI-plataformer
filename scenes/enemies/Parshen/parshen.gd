90bf3b937823576cae98d618ef650083901598fe
Danitinhg <dani111hg@gmail.com>
2025-11-07 15:20:24 +0100

    Eliminación de animaciones de parshen

--- scenes/enemies/Parshen/parshen.gd
+++ scenes/enemies/Parshen/parshen.gd
@@ -6,16 +6,16 @@ extends EnemyBase
 @onready var kyanon = $Toge_Kyanon
 
 #func _ready():
-    #shoot_timer.timeout.connect(_on_shoot_timer_timeout)
+	#shoot_timer.timeout.connect(_on_shoot_timer_timeout)
 
 func _on_shoot_timer_timeout():
-    if projectile_scene:
-        var projectile = projectile_scene.instantiate()
+	if projectile_scene:
+		var projectile = projectile_scene.instantiate()
 
-        get_tree().current_scene.add_child(projectile)
-        projectile.global_position = kyanon.global_position
+		get_tree().current_scene.add_child(projectile)
+		projectile.global_position = kyanon.global_position
 
-        if animated_sprite.flip_h:
-            projectile.direction = Vector2.LEFT
-        else:
-            projectile.direction = Vector2.RIGHT
+		if animated_sprite.flip_h:
+			projectile.direction = Vector2.LEFT
+		else:
+			projectile.direction = Vector2.RIGHT
