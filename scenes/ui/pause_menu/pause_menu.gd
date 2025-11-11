9e704760cdd5aa14c5cbc8dc8fd801fb650865e9
Danitinhg <dani111hg@gmail.com>
2025-11-11 12:07:52 +0100

    Añadido del boton opciones al menu de pausa, se muestran distintas opciones segun en donde este situado el player (en level_selector o en alguno de los niveles)

--- scenes/ui/pause_menu/pause_menu.gd
+++ scenes/ui/pause_menu/pause_menu.gd
@@ -1,17 +1,48 @@
 extends CanvasLayer
 
+enum Context { LEVEL, LEVEL_SELECTOR }
+
 @onready var panel = $Panel
 @onready var resume_button = $Panel/VBoxContainer/ResumeButton
 @onready var restart_button = $Panel/VBoxContainer/RestartButton
+@onready var options_button = $Panel/VBoxContainer/OptionsButton
 @onready var main_menu_button = $Panel/VBoxContainer/MainMenuButton
+
+var current_context: Context = Context.LEVEL
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	hide()
 
+	#en que entorno esta el player
+	detect_context()
+
+	#enseñar unos botones u otros segun si el player esta en un nivel o en el mapa
+	configure_buttons()
+
 	resume_button.pressed.connect(_on_resume_pressed)
 	restart_button.pressed.connect(_on_restart_pressed)
+	options_button.pressed.connect(_on_options_pressed)
 	main_menu_button.pressed.connect(_on_main_menu_pressed)
+
+func detect_context():
+	if GameManager.current_level == -1:
+		var scene_name = get_tree().current_scene.name
+		if scene_name == "LevelSelector":
+			current_context = Context.LEVEL_SELECTOR
+		else:
+			current_context = Context.LEVEL
+	else:
+		current_context = Context.LEVEL
+
+func configure_buttons():
+	match current_context:
+		Context.LEVEL:
+			restart_button.visible = true
+			options_button.visible = false
+		Context.LEVEL_SELECTOR:
+			restart_button.visible = false
+			options_button.visible = true
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -35,6 +66,9 @@ func _on_resume_pressed():
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+func _on_options_pressed():
+	pass
 
 func _on_main_menu_pressed():
 	get_tree().paused = false
